9c22abac0f8e72778b20909343ffcae2458aab6a
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-07-31 02:32:06 +0530

    Add MUI styling

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import './App.css';
+
 import {
   BrowserRouter as Router,
   Routes,
@@ -6,16 +8,22 @@ import {
 import Settings from './pages/Settings';
 import Questions from './pages/Questions';
 import FinalScreen from './pages/FinalScreen';
+import { Container } from '@mui/material';
+import { Box } from '@mui/system';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route exact path="/" element={<Settings />} />
-        <Route exact path="/questions" element={<Questions />} />
-        <Route exact path="/score" element={<FinalScreen />} />
-      </Routes>
-    </Router>
+      <Container maxWidth="sm">
+        <Box mt={5}>
+          <Routes>
+            <Route exact path="/" element={<Settings />} />
+            <Route exact path="/questions" element={<Questions />} />
+            <Route exact path="/score" element={<FinalScreen />} />
+          </Routes>
+        </Box>
+      </Container>
+    </Router >
   );
 }
 
