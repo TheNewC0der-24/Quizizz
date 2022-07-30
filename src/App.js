afc8a59c7a4b0235934dd75fd0ccf419b8b43300
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-07-31 00:57:38 +0530

    Routing between pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+import Settings from './pages/Settings';
+import Questions from './pages/Questions';
+import FinalScreen from './pages/FinalScreen';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Settings />} />
+        <Route exact path="/questions" element={<Questions />} />
+        <Route exact path="/score" element={<FinalScreen />} />
+      </Routes>
+    </Router>
   );
 }
 
