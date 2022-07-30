bdce864bf161599243d419eade5270cb3e2daf4e
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-07-31 02:32:58 +0530

    Create Card

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,6 +1,27 @@
+import { Card, CardHeader, CardContent, Button } from "@mui/material";
+import SelectField from "../components/SelectField";
+import { Box } from "@mui/system";
+
 const Settings = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (
-        <div>Settings</div>
+        <Card>
+            <CardHeader sx={{ textAlign: "center" }} title="QUIZ App" />
+            <CardContent>
+                <form onSubmit={handleSubmit}>
+                    <SelectField label="Category" />
+                    <SelectField label="Difficulty" />
+                    <SelectField label="Type" />
+                    <Box mt={3} textAlign="center">
+                        <Button variant="contained" type="submit">Get Started</Button>
+                    </Box>
+                </form>
+            </CardContent>
+        </Card>
     )
 }
 
