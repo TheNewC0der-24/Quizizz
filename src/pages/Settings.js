b5183ac53a1b5ea553c435ef8dc62bf59d652d02
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-08-05 02:17:10 +0530

    Setting up the options

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,9 +1,39 @@
-import { Card, CardHeader, CardContent, Button } from "@mui/material";
+import { Card, CardHeader, CardContent, Button, CircularProgress, Typography } from "@mui/material";
 import SelectField from "../components/SelectField";
 import { Box } from "@mui/system";
 import TextField from "../components/TextField";
+import useAxios from "../hooks/useAxios";
 
 const Settings = () => {
+
+    const { response, error, loading } = useAxios({ url: "/api_category.php" });
+
+    if (loading) {
+        return (
+            <Box style={{ display: 'flex', justifyContent: 'center' }} mt={20}>
+                <CircularProgress />
+            </Box>
+        )
+    }
+
+    if (error) {
+        return (
+            <Typography variant="h6" mt={20} color="red">
+                Something went wrong !
+            </Typography>
+        )
+    }
+
+    const difficultyOptions = [
+        { id: "easy", name: "Easy" },
+        { id: "medium", name: "Medium" },
+        { id: "hard", name: "Hard" },
+    ];
+
+    const typeOptions = [
+        { id: "multiple", name: "Multiple Choice" },
+        { id: "boolean", name: "True/False" },
+    ];
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -14,9 +44,9 @@ const Settings = () => {
             <CardHeader sx={{ textAlign: "center" }} title="QUIZ App" />
             <CardContent>
                 <form onSubmit={handleSubmit}>
-                    <SelectField label="Category" />
-                    <SelectField label="Difficulty" />
-                    <SelectField label="Type" />
+                    <SelectField options={response.trivia_categories} label="Category" />
+                    <SelectField options={difficultyOptions} label="Difficulty" />
+                    <SelectField options={typeOptions} label="Type" />
                     <TextField />
                     <Box mt={3} textAlign="center">
                         <Button variant="contained" type="submit">Get Started</Button>
