418ee266ca1727645eff3541883968624c640e36
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-08-06 21:45:00 +0530

    Create initial state in reducer

---
+++ src/redux/reducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    question_category: '',
+    question_difficulty: '',
+    question_type: '',
+    amount_of_question: '50',
+    score: 0
+}
+
+const reducer = (state = initialState, action) => {
+    return state;
+};
+
+export default reducer;
