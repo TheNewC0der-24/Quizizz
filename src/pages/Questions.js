37dfdde22c5a1ae49bf5dffea5034a79736f2f0b
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-08-29 03:55:47 +0530

    Fetch Options and set score functionality

--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -1,17 +1,27 @@
-import React from "react";
-import { Button, CircularProgress, Typography } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { Button, CircularProgress, Typography, Divider } from "@mui/material";
 import { Box } from "@mui/system";
-
 import useAxios from "../hooks/useAxios";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { handleScoreChange } from "../redux/actions";
+
+const getRandomInt = (max) => {
+    return Math.floor(Math.random() * Math.floor(max));
+}
 
 const Questions = () => {
+
+    const navigate = useNavigate();
+
+    const dispatch = useDispatch();
 
     const {
         question_category,
         question_difficulty,
         question_type,
-        amount_of_question
+        amount_of_question,
+        score
     } = useSelector(state => state);
 
     let apiUrl = `/api.php?amount=${amount_of_question}`;
@@ -26,6 +36,19 @@ const Questions = () => {
     }
 
     const { response, loading } = useAxios({ url: apiUrl })
+    const [questionIndex, setQuestionIndex] = useState(0);
+    const [options, setOptions] = useState([]);
+    console.log('options', options);
+
+
+    useEffect(() => {
+        if (response) {
+            const question = response.results[questionIndex];
+            let answers = [...question.incorrect_answers];
+            answers.splice(getRandomInt(question.incorrect_answers.length), 0, question.correct_answer);
+            setOptions(answers);
+        }
+    }, [response, questionIndex]);
 
     if (loading) {
         return (
@@ -35,23 +58,39 @@ const Questions = () => {
         )
     }
 
+    const handleAnswer = (e) => {
+        const question = response.results[questionIndex];
+        if (e.target.textContent === question.correct_answer) {
+            dispatch(handleScoreChange(score + 1));
+        }
+
+        if (questionIndex + 1 < response.results.length) {
+            setQuestionIndex(questionIndex + 1);
+        } else {
+            navigate('/score');
+        }
+    }
+
     return (
         <Box>
-            <Typography textAlign={"center"} variant="h4">
-                Questions
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <Typography item textAlign={"center"} variant="h4">
+                    Question - {questionIndex + 1}
+                </Typography>
+                <div>
+                    <Button variant="contained">Score : {score} / {response.results.length}</Button>
+                </div>
+            </div>
+            <br />
+            <Divider />
+            <Typography mt={5} mb={3}>
+                {response.results[questionIndex].question}
             </Typography>
-            <Typography mt={5}>
-                This is the questions page.
-            </Typography>
-            <Box mt={2}>
-                <Button variant="contained">Answer 1</Button>
-            </Box>
-            <Box mt={2}>
-                <Button variant="contained">Answer 2</Button>
-            </Box>
-            <Box mt={5}>
-                <Button>Score: 2/6</Button>
-            </Box>
+            {options.map((data, id) => (
+                <Box mt={2} key={id}>
+                    <Button onClick={handleAnswer} variant="contained">{data}</Button>
+                </Box>
+            ))}
         </Box>
     )
 }
