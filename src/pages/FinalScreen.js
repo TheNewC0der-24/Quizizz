9cf705d14ed53c8e570330478f02311b0f55acab
Bhavya Khurana <69009796+TheNewC0der-24@users.noreply.github.com>
2022-10-20 03:17:01 +0530

    Create the Final Score Screen

--- src/pages/FinalScreen.js
+++ src/pages/FinalScreen.js
@@ -1,8 +1,31 @@
 import React from "react";
+import { Box, Button, Typography } from "@mui/material";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { handleAmountChange, handleScoreChange } from "../redux/actions";
 
 const FinalScreen = () => {
+
+    const { score } = useSelector(state => state);
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleBack = () => {
+        dispatch(handleScoreChange(0)); // reset score
+        dispatch(handleAmountChange(50)); // reset amount of questions
+        navigate("/"); // go back to home page
+    }
+
     return (
-        <div>FinalScreen</div>
+        <Box mt={30}>
+            <Typography variant="h3" align="center" mb={3}>Final Score : {score}</Typography>
+            <Box textAlign="center">
+                <Button color="warning" variant="contained" onClick={handleBack}><ArrowBackIcon /> Back to Settings!</Button>
+            </Box>
+        </Box>
     )
 }
 
